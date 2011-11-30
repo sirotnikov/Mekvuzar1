f70b14da4fb763858de52a0ed1d17b0cd4195180
Dima <Dima@132.68.205.43>
2011-11-30 18:20:35 +0200

    renamed exceptions

---
+++ ex1/src/ex1/UnknownException.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package ex1;
+
+/**
+ * @author Dima
+ *
+ */
+public class UnknownException extends CellException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5301262892718146212L;
+
+}
