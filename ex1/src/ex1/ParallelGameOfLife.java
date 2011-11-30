07fdc4d751a0ee181ace28f3ec1c2b9917860e36
Dima <Dima@132.68.205.43>
2011-11-30 16:16:14 +0200

    base

---
+++ ex1/src/ex1/ParallelGameOfLife.java
@@ -0,0 +1,11 @@
+package ex1;
+
+public class ParallelGameOfLife implements GameOfLife {
+
+	public boolean[][] invoke(boolean[][] initalField, int hSplit, int vSplit,
+			int generations) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
