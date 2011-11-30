5bdfca18d43fb2ebcd05cee862edd30f41dc699c
Dima <Dima@132.68.205.43>
2011-11-30 18:07:34 +0200

    created empty Parallel Board and Section

---
+++ ex1/src/ex1/ParallelBoard.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package ex1;
+
+/**
+ * @author Dima
+ *
+ */
+public class ParallelBoard {
+	Section[][] _sectionsArray;	//Should eventually be Section[_vSections][_hSections];
+	int _totalWidth;
+	int _totalHeight;
+	int _hSections;
+	int _vSections;
+	
+	
+}
---
+++ ex1/src/ex1/Section.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package ex1;
+
+/**
+ * @author Dima
+ *
+ */
+public class Section {
+	Cell[][] _cellsArray;
+	int _width;
+	int _height;
+	int _xOffset;
+	int _yOffset;
+	int _totalBoardWidth;
+	int _totalBoardHeight;
+
+}
