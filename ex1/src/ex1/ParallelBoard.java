21fd2450fd5abff46a0b5a19dea329ef265437fa
solaris <solaris@132.68.176.44>
2011-11-30 19:38:47 +0200

    eran 30/11 19:38

--- ex1/src/ex1/ParallelBoard.java
+++ ex1/src/ex1/ParallelBoard.java
@@ -19,8 +19,36 @@ public class ParallelBoard {
 		_totalWidth = boardWidth;
 		_hSections = hSections;
 		_vSections = vSections;
+		int normalSecHgtSize = boardHeight / hSections;
+		int normalSecWdtSize = boardWidth / vSections;
+		int lastSecHgtSize = boardHeight % hSections;
+		int lastSecWdtSize = boardWidth % vSections;
+		for(int i=0;i<hSections;i++)
+			for(int j=0;j<vSections;j++){
+				_sectionsArray[i][j].setTotalBoardHeight(boardHeight);
+				_sectionsArray[i][j].setTotalBoardWidth(boardWidth);
+				_sectionsArray[i][j].setXOffset(i*normalSecHgtSize);
+				_sectionsArray[i][j].setYOffset(normalSecWdtSize);
+				if(i != hSections-1){
+					_sectionsArray[i][j].setHeight(normalSecHgtSize); 
+				}
+				else
+				{
+					_sectionsArray[i][j].setHeight(lastSecHgtSize); 
+				}
+				if(i != hSections-1){
+					_sectionsArray[i][j].setWidth(normalSecWdtSize);
+				}
+				else
+				{
+					_sectionsArray[i][j].setWidth(lastSecWdtSize);
+				}
+			}
 		
-		sectionCopy(Cell sourceBoard,Cell targetBoard[][])
+		
 	}
-	GetSections(int boardHeight,int boardWidth,int hSections,int vSections,)
+	public boolean sectionCopy(CellImpl sourceBoard,CellImpl targetBoard[][]){
+		return true;
+	}
+
 }
--- ex1/src/ex1/Section.java
+++ ex1/src/ex1/Section.java
@@ -16,10 +16,46 @@ public class Section {
 	int _totalBoardWidth;
 	int _totalBoardHeight;
 	public Section(int height,int width){
-		_cellsArray = new Cell[height][width];
+		_cellsArray = new CellImpl[height][width];
 		_width = width;
 		_height = height;
 		
+	}
+	public int getWidth(){
+		return _width;
+	}
+	public int getHeight(){
+		return _height;
+	}
+	public int getXOffset(){
+		return _xOffset;
+	}
+	public int getYOffset(){
+		return _yOffset;
+	}
+	public int getTotalBoardWidth(){
+		return _totalBoardWidth;
+	}
+	public int getTotalBoardHeight(){
+		return _totalBoardHeight;
+	}
+	public void setWidth(int newWidth){
+		_width = newWidth;
+	}
+	public void setHeight(int newHeight){
+		_height = newHeight;
+	}
+	public void setXOffset(int newXOffset){
+		_xOffset = newXOffset;
+	}
+	public void setYOffset(int newYoffset){
+		_yOffset = newYoffset;
+	}
+	public void setTotalBoardWidth(int newTotalBoardWidth){
+		_totalBoardWidth = newTotalBoardWidth;
+	}
+	public void setTotalBoardHeight(int newTotalBoardHeight){
+		 _totalBoardHeight = newTotalBoardHeight;
 	}
 
 }
