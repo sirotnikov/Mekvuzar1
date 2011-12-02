d8da9805f2a8d7f233245c065a3514f81db8ac44
Dima <Dima@132.68.205.76>
2011-12-02 06:50:11 +0200

    improved functions

--- ex1/src/ex1/Directions.java
+++ ex1/src/ex1/Directions.java
@@ -6,33 +6,36 @@ package ex1;
 /**
  * @author Dima
  *
- */
-public enum Directions {
+ */public enum Directions {
 	NORTH_WEST {
 		public int newX(int x) { return x - 1;}
 		public int newY(int y) { return y - 1;}
 	},
 	NORTH {
 		public int newY(int y) {return y - 1;}
+		public int newX(int x) {return x;}
 	}, 
 	NORTH_EAST {
 		public int newY(int y) {return y - 1;}
 		public int newX(int x) {return x + 1;}
 		
 	}, EAST {
+		public int newY(int y) {return y;}
 		public int newX(int x) {return x + 1;}
 	}, SOUTH_EAST {
 		public int newY(int y) {return y + 1;}
 		public int newX(int x) {return x + 1;}
 	}, SOUTH {
+		public int newX(int x) {return x;}
 		public int newY(int y) {return y + 1;}
 	}, SOUTH_WEST {
-		public int newY(int y) {return y + 1;}
+		public int newY(int y) {return y + 1;}	
 		public int newX(int x) {return x - 1;}
 	}, WEST {
 		public int newX(int x) {return x - 1;}
+		public int newY(int y) {return y;}
 	};
 	
-	public int newX(int x){ return x;}
-	public int newY(int y){ return y;}
+	public int newX(int x) { System.out.println("======== NEW_X_ DEFAULT ========"); return x;}
+	public int newY(int y) { System.out.println("======== NEW_Y_ DEFAULT ========"); return y;}
 }
